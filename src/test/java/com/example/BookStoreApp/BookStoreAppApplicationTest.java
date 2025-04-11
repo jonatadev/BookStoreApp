377bcfd08846a29a3d0c62d0693df4a72b961076
jonatadev <jonatadev@gmail.com>
2025-04-11 10:46:06 -0300

    feature/2025005 feat: job tests

---
+++ src/test/java/com/example/BookStoreApp/BookStoreAppApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.BookStoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAppApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
